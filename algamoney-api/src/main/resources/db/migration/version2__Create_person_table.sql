951ef1399d104f62b5e01069eaa30feb7572147c
P Mukuna <pcishiku@gmail.com>
2020-07-07 23:02:06 -0300

    Update and rename version2__Create_person_table.sql to V2__Insert_firsts_persons.sql

--- algamoney-api/src/main/resources/db/migration/version2__Create_person_table.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO algamoneyapi.categoria (CODIGO,NOME) VALUES(1,'OBERSON');
-INSERT INTO algamoneyapi.categoria(CODIGO,NOME) VALUES(2,'LOICE');
-INSERT INTOalgamoneyapi.categoria (CODIGO,NOME) VALUES(3,'GAD');
